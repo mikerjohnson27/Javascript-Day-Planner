9d42c19571ffd7803f8322c9dee90d435db1b6f3
Mike Johnson <mikerjohnson27unh@gmail.com>
2022-07-12 16:04:21 -0400

    adding clear all button

--- js/script.js
+++ js/script.js
@@ -5,44 +5,44 @@ $("#today").text(moment().format('llll'))
 
 
 $("#save").click(function(){
-    text = $("#am7").val();
+    let text = $("#am7").val();
     console.log(text);
-    hour = $(this).siblings(".hour").text();
+    let hour = $(this).siblings(".hour").text();
     console.log(hour);
     localStorage.setItem(hour, JSON.stringify(text));
 });
 $("#save1").click(function(){
-    text = $("#am9").val();
+    let text = $("#am9").val();
     console.log(text);
-    hour = $(this).siblings(".hour1").text();
+    let hour = $(this).siblings(".hour1").text();
     console.log(hour);
     localStorage.setItem(hour, JSON.stringify(text));
 });
 $("#save2").click(function(){
-    text = $("#am11").val();
+    let text = $("#am11").val();
     console.log(text);
     hour = $(this).siblings(".hour2").text();
     console.log(hour);
     localStorage.setItem(hour, JSON.stringify(text));
 });
 $("#save3").click(function(){
-    text = $("#pm1").val();
+    let text = $("#pm1").val();
     console.log(text);
-    hour = $(this).siblings(".hour3").text();
+    let hour = $(this).siblings(".hour3").text();
     console.log(hour);
     localStorage.setItem(hour, JSON.stringify(text));
 });
 $("#save4").click(function(){
-    text = $("#pm3").val();
+    let text = $("#pm3").val();
     console.log(text);
-    hour = $(this).siblings(".hour4").text();
+    let hour = $(this).siblings(".hour4").text();
     console.log(hour);
     localStorage.setItem(hour, JSON.stringify(text));
 });
 $("#save5").click(function(){
-    text = $("#pm5").val();
+    let text = $("#pm5").val();
     console.log(text);
-    hour = $(this).siblings(".hour5").text();
+    let hour = $(this).siblings(".hour5").text();
     console.log(hour);
     localStorage.setItem(hour, JSON.stringify(text));
 });
@@ -52,40 +52,42 @@ $("#clear").click(function(){
     text = $("#am7").val("");
     text = $("#am7").val();
     hour = $(this).siblings(".hour").text();
-    localStorage.setItem(hour, JSON.stringify(text));
+    localStorage.removeItem(hour, JSON.stringify(text));
 });
 $("#clear1").click(function(){
     text = $("#am9").val("");
     text = $("#am9").val();
     hour = $(this).siblings(".hour1").text();
-    localStorage.setItem(hour, JSON.stringify(text));
+    localStorage.removeItem(hour, JSON.stringify(text));
 });
 $("#clear2").click(function(){
     text = $("#am11").val("");
     text = $("#am11").val();
     hour = $(this).siblings(".hour2").text();
-    localStorage.setItem(hour, JSON.stringify(text));
+    localStorage.removeItem(hour, JSON.stringify(text));
 });
 $("#clear3").click(function(){
     text = $("#pm1").val("");
     text = $("#pm1").val();
     hour = $(this).siblings(".hour3").text();
-    localStorage.setItem(hour, JSON.stringify(text));
+    localStorage.removeItem(hour, JSON.stringify(text));
 });
 $("#clear4").click(function(){
     text = $("#pm3").val("");
     text = $("#pm3").val();
     hour = $(this).siblings(".hour4").text();
-    localStorage.setItem(hour, JSON.stringify(text));
+    localStorage.removeItem(hour, JSON.stringify(text));
 });
 $("#clear5").click(function(){
     text = $("#pm5").val("");
     text = $("#pm5").val();
     hour = $(this).siblings(".hour5").text();
-    localStorage.setItem(hour, JSON.stringify(text));
+    localStorage.removeItem(hour, JSON.stringify(text));
 });
 
-
-// function savetext(){
-//     let saveEventText = 
-// };
+$("#view").click(function(){
+    text = $("#am7").val("");
+    text = $("#am7").val();
+    hour = $(this).siblings(".hour").text();
+    localStorage.getItem(hour, JSON.stringify(text)); 
+});
