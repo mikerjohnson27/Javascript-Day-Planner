8fc4b6cb7361761f2c8255eb0e2e9a82a7d4ed73
Mike Johnson <mikerjohnson27unh@gmail.com>
2022-07-18 14:03:17 -0400

    adding localstorage object to change the JS.

--- js/script.js
+++ js/script.js
@@ -1,104 +1,34 @@
-
-let m = moment();
-
+let m = moment()
 $("#today").text(moment().format('llll'))
 let hour = $(this).siblings(".hour1").text();
 
-function loadStorage(){
-    if(localStorage.getItem((hour) !== null)){
+let sevenAM = document.getElementById('am7').value
+let nineAM = document.getElementById('am9').value
+let elevenAM = document.getElementById('am11').value
+let onePM = document.getElementById('pm1').value
+let threePM = document.getElementById('pm3').value
+let fivePM = document.getElementById('pm5').value
+
+let LocalStorageObj = {
+    sevenAMLSO: JSON.stringify(sevenAM),
+    nineAMLSO: JSON.stringify(nineAM),
+    elevenAMLSO: JSON.stringify(elevenAM),
+    oneAMLSO: JSON.stringify(onePM),
+    threeAMLSO: JSON.stringify(threePM),
+    fiveAMLSO: JSON.stringify(fivePM),
+    };
+
+let LocalStorageObj_String = JSON.stringify(LocalStorageObj)
+
+localStorage.setItem("schedule", LocalStorageObj_String)
+console.log(localStorage)
+
+let LocalStorageObj_DeString = JSON.parse(LocalStorageObj_String)
+console.log(LocalStorageObj_DeString)
+
+
+function createLocalStorageObj(){
+    if(localStorage.getItem(LocalStorageObj) !== null){
         console.log("local storage is not available.")
-    }else{
-        const storage = localStorage.getItem('7 AM', '9 AM', '11 AM', '1 PM', '3 PM', '5 PM')
-        document.querySelector('#am7', 'am9', '#am11', '#1pm', '#3pm', '#5pm').textContent = JSON.parse(storage)
+        };
     }
-}
-loadStorage();
-
-
-$("#save").click(function(){
-    let text = $("#am7").val();
-    console.log(text);
-    let hour = $(this).siblings(".hour").text();
-    console.log(hour);
-    localStorage.setItem(hour, JSON.stringify(text));
-});
-$("#save1").click(function(){
-    let text = $("#am9").val();
-    console.log(text);
-    let hour = $(this).siblings(".hour1").text();
-    console.log(hour);
-    localStorage.setItem(hour, JSON.stringify(text));
-});
-$("#save2").click(function(){
-    let text = $("#am11").val();
-    console.log(text);
-    hour = $(this).siblings(".hour2").text();
-    console.log(hour);
-    localStorage.setItem(hour, JSON.stringify(text));
-});
-$("#save3").click(function(){
-    let text = $("#pm1").val();
-    console.log(text);
-    let hour = $(this).siblings(".hour3").text();
-    console.log(hour);
-    localStorage.setItem(hour, JSON.stringify(text));
-});
-$("#save4").click(function(){
-    let text = $("#pm3").val();
-    console.log(text);
-    let hour = $(this).siblings(".hour4").text();
-    console.log(hour);
-    localStorage.setItem(hour, JSON.stringify(text));
-});
-$("#save5").click(function(){
-    let text = $("#pm5").val();
-    console.log(text);
-    let hour = $(this).siblings(".hour5").text();
-    console.log(hour);
-    localStorage.setItem(hour, JSON.stringify(text));
-});
-
-//Clear Functions
-$("#clear").click(function(){
-    text = $("#am7").val("");
-    text = $("#am7").val();
-    hour = $(this).siblings(".hour").text();
-    localStorage.removeItem(hour, JSON.stringify(text));
-});
-$("#clear1").click(function(){
-    text = $("#am9").val("");
-    text = $("#am9").val();
-    hour = $(this).siblings(".hour1").text();
-    localStorage.removeItem(hour, JSON.stringify(text));
-});
-$("#clear2").click(function(){
-    text = $("#am11").val("");
-    text = $("#am11").val();
-    hour = $(this).siblings(".hour2").text();
-    localStorage.removeItem(hour, JSON.stringify(text));
-});
-$("#clear3").click(function(){
-    text = $("#pm1").val("");
-    text = $("#pm1").val();
-    hour = $(this).siblings(".hour3").text();
-    localStorage.removeItem(hour, JSON.stringify(text));
-});
-$("#clear4").click(function(){
-    text = $("#pm3").val("");
-    text = $("#pm3").val();
-    hour = $(this).siblings(".hour4").text();
-    localStorage.removeItem(hour, JSON.stringify(text));
-});
-$("#clear5").click(function(){
-    text = $("#pm5").val("");
-    text = $("#pm5").val();
-    hour = $(this).siblings(".hour5").text();
-    localStorage.removeItem(hour, JSON.stringify(text));
-});
-
-$("#view").click(function(){
-    text = $("#am7").val("");
-    text = $("#am7").val();
-    hour = $(this).siblings("7 AM").text();
-
-});
