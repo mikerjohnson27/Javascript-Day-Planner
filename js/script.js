f2bd13ff2e3911eb8e7083bbea27ec95012dc46e
Mike Johnson <mikerjohnson27unh@gmail.com>
2022-07-19 12:59:24 -0400

    adding side panel for displaying data.

--- js/script.js
+++ js/script.js
@@ -6,64 +6,30 @@ const saveBtn = document.getElementById('save')
 const viewBtn = document.getElementById('view')
 
 const saveListen = document.addEventListener('click', store, saveBtn)
-const displayListen = document.addEventListener('click', display, viewBtn)
 
-let sevenAM = JSON.stringify(document.getElementById('am7'))
-let nineAM = JSON.stringify(document.getElementById('am9'))
-let elevenAM = JSON.stringify(document.getElementById('am11'))
-let onePM = JSON.stringify(document.getElementById('pm1'))
-let threePM = JSON.stringify(document.getElementById('pm3'))
-let fivePM = JSON.stringify(document.getElementById('pm5'))
-const key = 'schedule'
+function store(){
+    sevenAM = document.getElementById('am7').value;
+    nineAM = document.getElementById('am9').value;
+    elevenAM = document.getElementById('am11').value;
+    onePM = document.getElementById('pm1').value;
+    threePM = document.getElementById('pm3').value;
+    fivePM = document.getElementById('pm5').value;
 
-function store(LocalStorageObj){
-    if(LocalStorageObj == true){
-        let sevenAM = document.getElementById('am7').value
-        let nineAM = document.getElementById('am9').value
-        let elevenAM = document.getElementById('am11').value
-        let onePM = document.getElementById('pm1').value
-        let threePM = document.getElementById('pm3').value
-        let fivePM = document.getElementById('pm5').value
+    const LocalStorageObj = {
+        sevenAM: sevenAM,
+        nineAM: nineAM,
+        elevenAM: elevenAM,
+        onePM: onePM,
+        threePM: threePM,
+        fivePM: fivePM,
+        };
 
-        LocalStorageObj = {
-            sevenAMLSO: sevenAM,
-            nineAMLSO: nineAM,
-            elevenAMLSO: elevenAM,
-            onePMLSO: onePM,
-            threePMLSO: threePM,
-            fivePMLSO: fivePM
-            }
-            localStorage.setItem(key, JSON.stringify(LocalStorageObj));
-            console.log(localStorage.getItem(key, JSON.parse(LocalStorageObj)))
-        }else{
-            
-            let sevenAM = document.getElementById('am7').value
-            let nineAM = document.getElementById('am9').value
-            let elevenAM = document.getElementById('am11').value
-            let onePM = document.getElementById('pm1').value
-            let threePM = document.getElementById('pm3').value
-            let fivePM = document.getElementById('pm5').value
-            
-            window.localStorage.removeItem(key)
 
-            LocalStorageObj = {
-                sevenAMLSO: sevenAM,
-                nineAMLSO: nineAM,
-                elevenAMLSO: elevenAM,
-                onePMLSO: onePM,
-                threePMLSO: threePM,
-                fivePMLSO: fivePM
-                }
-                window.localStorage.setItem(key, JSON.stringify(LocalStorageObj));
-                console.log(JSON.parse(window.localStorage.getItem(key)))
-            }
-    }
-
-function display(LocalStorageObj){
-    sevenAM = JSON.parse(localStorage.getItem(key))
-    nineAM = JSON.parse(localStorage.getItem(key))
-    elevenAM = JSON.parse(localStorage.getItem(key))
-    onePM = JSON.parse(localStorage.getItem(key))
-    threePM = JSON.parse(localStorage.getItem(key))
-    fivePM = JSON.parse(localStorage.getItem(key))
+    window.localStorage.setItem('schedule', JSON.stringify(LocalStorageObj));
+    console.log(window.localStorage.getItem('schedule', JSON.stringify(LocalStorageObj)));
+}
+    
+window.onload = function (LocalStorageObj){
+    let getObj = localStorage.getItem('schedule');
+    console.log('getObj', JSON.parse(getObj))
 };
