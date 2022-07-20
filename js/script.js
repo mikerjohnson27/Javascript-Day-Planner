1df4a77b998a8e6934f2ff33a6c643443a9a8d71
Mike Johnson <mikerjohnson27unh@gmail.com>
2022-07-20 16:24:07 -0400

    minor additions.

--- js/script.js
+++ js/script.js
@@ -2,34 +2,52 @@ let m = moment()
 $("#today").text(moment().format('llll'))
 let hour = $(this).siblings(".hour1").text();
 
-const saveBtn = document.getElementById('save')
-const viewBtn = document.getElementById('view')
+nineAM = document.getElementById('am9')
+tenAM = document.getElementById('am10')
+elevenAM = document.getElementById('am11')
+twelvePM = document.getElementById('pm12')
+onePM = document.getElementById('pm1')
+twoPM = document.getElementById('pm2')
+threePM = document.getElementById('pm3')
+fourPM = document.getElementById('pm4')
+fivePM = document.getElementById('pm5')
 
-const saveListen = document.addEventListener('click', store, saveBtn)
+document.getElementById('hour')
 
-function store(){
-    sevenAM = document.getElementById('am7').value;
-    nineAM = document.getElementById('am9').value;
-    elevenAM = document.getElementById('am11').value;
-    onePM = document.getElementById('pm1').value;
-    threePM = document.getElementById('pm3').value;
-    fivePM = document.getElementById('pm5').value;
+nineAM.addEventListener('Composition', nineAM, store);
+document.addEventListener('Composition', tenAM, store);
+document.addEventListener('Composition', elevenAM, store);
+document.addEventListener('Composition', twelvePM, store);
+document.addEventListener('Composition', onePM, store);
+document.addEventListener('Composition', twoPM, store);
+document.addEventListener('Composition', threePM, store);
+document.addEventListener('Composition', fourPM, store);
+document.addEventListener('Composition', fivePM, store);
 
-    const LocalStorageObj = {
-        sevenAM: sevenAM,
-        nineAM: nineAM,
-        elevenAM: elevenAM,
-        onePM: onePM,
-        threePM: threePM,
-        fivePM: fivePM,
+
+function store(e){
+        const LocalStorageObj = {
+            nineAM: e.nineAM.value,
+            tenAM: tenAM,
+            elevenAM: elevenAM,
+            twelvePM: twelvePM,
+            onePM: onePM,
+            twoPM: onePM,
+            threePM: threePM,
+            threePM: threePM,
+            fivePM: fivePM,
         };
-
-
     window.localStorage.setItem('schedule', JSON.stringify(LocalStorageObj));
     console.log(window.localStorage.getItem('schedule', JSON.stringify(LocalStorageObj)));
 }
     
-window.onload = function (LocalStorageObj){
-    let getObj = localStorage.getItem('schedule');
-    console.log('getObj', JSON.parse(getObj))
+window.onload = function (){
+    let Obj = window.localStorage.getItem('schedule', JSON.parse('schedule'[localStorageObj]));
+    console.log(Obj['nineAM'])
 };
+
+document.save
+
+$('#view').click(function(){
+    $("#planner").removeClass('.black').addClass("red")
+});
